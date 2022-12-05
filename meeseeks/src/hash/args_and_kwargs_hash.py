4e143d0f7909fc2da9633e6cab2d3c945726e31a
Ximit Gaia <im.ximit@gamil.com>
2022-12-05 20:51:59 -0300

    fix: hash logic

--- meeseeks/src/hash/args_and_kwargs_hash.py
+++ meeseeks/src/hash/args_and_kwargs_hash.py
@@ -3,7 +3,6 @@ import hashlib
 
 def hash_generator(*args, **kwargs) -> str:
     hash_args = [str(arg) for arg in args]
-    hash_args.sort()
     hash_kwargs = [f"{str(key)}{str(arg)}" for key, arg in kwargs.items()]
     hash_kwargs.sort()
     hash_instance = hashlib.sha1(f"{hash_args}{hash_kwargs}".encode())
--- tests/hash/test_iam_hash_generator.py
+++ tests/hash/test_iam_hash_generator.py
@@ -19,6 +19,12 @@ def test_hash_generator_args_not_equal():
     assert hash_2 != hash_1
 
 
+def test_hash_generator_args_not_equal_different_order():
+    hash_1 = hash_generator(2, 1)
+    hash_2 = hash_generator(1, 2)
+    assert hash_2 != hash_1
+
+
 def test_hash_generator_kwargs_and_kwargs_reverse():
     hash_1 = hash_generator(a=1, b=2)
     hash_2 = hash_generator(b=2, a=1)
@@ -35,3 +41,4 @@ def test_hash_generator_kwargs_not_equal():
     hash_1 = hash_generator(a=1, b=2)
     hash_2 = hash_generator(b=1, a=2)
     assert hash_2 != hash_1
+
